e3d7cc5adc8cb1e8eaabb629198abee3e147c0ef
Mark A Emmons <MarkAndreasEmmons@gmail.com>
2020-07-09 20:32:42 -0500

    Move cursor to end of the line when searching history

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,11 @@
 # Default location
-Set-Location -Path "C:\Users\Mark\Documents"
+Set-Location -Path "$Home\Documents"
+Function dc { Set-Location -Path "$Home\Documents" }
 
-Invoke-Item -Path 'AutoHotKey\VimBindings.ahk'
-
-Import-Module -Name PSReadline
+# Hotkeys
+If(Get-Process -Name AutoHotkey -ErrorAction SilentlyContinue) {
+	Invoke-Item -Path 'AutoHotKey\VimBindings.ahk'
+}
 
 # Close with Ctrl+d
 Set-PSReadlineKeyHandler -Key Ctrl-d -Function DeleteCharOrExit
@@ -16,6 +18,8 @@ Set-PSReadlineOption -ShowToolTips
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd:$True
+
 # PERL-isms
 Function ql { $args }
 Function qs { "$args" }
@@ -23,3 +27,14 @@ Function qs { "$args" }
 # Open neovim
 Set-Alias vim 'C:\Program Files\Neovim\bin\nvim.exe'
 $vimrc = 'C:\Users\Mark\AppData\Local\nvim'
+
+Function dc { Set-Location -Path 'C:\Users\Mark\Documents' }
+
+### TODO ###
+# more consistent keybindings between term,posh,vim
+# fix vim cursor not being a block in normal mode // This is a conemu thing
+#	// insert should go to end of line
+# 
+# lifetimes / pointers
+# 
+# fzf
